2aabdbed12d3dc6345f8f012d41282495a2b1196
cyilin <cyilin@users.noreply.github.com>
2016-03-15 23:46:20 +0800

    update to 1.9

--- src/main/java/com/github/cyilin/SafeJoin/Main.java
+++ src/main/java/com/github/cyilin/SafeJoin/Main.java
@@ -1,51 +1,59 @@
 package com.github.cyilin.SafeJoin;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import net.ess3.api.IEssentials;
 import org.bukkit.ChatColor;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import net.ess3.api.IEssentials;
-import org.bukkit.World;
-import org.bukkit.configuration.InvalidConfigurationException;
-import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main extends JavaPlugin implements Listener, CommandExecutor {
 
-    private HashMap<String, Long> timestamp = new HashMap();
+    private HashMap<String, Long> timestamp;
     private List<String> worlds;
     private String command;
     private IEssentials ess;
     private int mode;
     private boolean enable;
+    private Set<String> tp;
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         String act = (args.length == 0 ? "" : args[0].toLowerCase());
-        sender.sendMessage(ChatColor.GOLD + "[SafeJoin] " + ChatColor.BLUE + "==SafeJoin "
-                + getServer().getPluginManager().getPlugin("SafeJoin").getDescription().getVersion() + "==");
-        sender.sendMessage(ChatColor.GOLD + "[SafeJoin] " + ChatColor.WHITE + "/safejoin reload");
-        sender.sendMessage(ChatColor.GOLD + "[SafeJoin] " + ChatColor.WHITE + "by cylin https://github.com/cyilin");
         if (act.equals("reload") && sender.hasPermission("safejoin.admin")) {
             sender.sendMessage(ChatColor.GOLD + "[SafeJoin] " + ChatColor.GREEN + "reload...");
             reload();
             sender.sendMessage(ChatColor.GOLD + "[SafeJoin] " + ChatColor.GREEN + "done.");
+        } else if (act.equals("tptospawn") && sender.hasPermission("safejoin.admin")) {
+            if (args.length == 2) {
+                this.tp.add(args[1].toLowerCase());
+            } else {
+                sender.sendMessage(ChatColor.GOLD + "[SafeJoin] " + ChatColor.WHITE + "/safejoin tptospawn [player]");
+            }
+            return true;
+        } else {
+            sender.sendMessage(ChatColor.GOLD + "[SafeJoin] " + ChatColor.BLUE + "==SafeJoin "
+                    + getServer().getPluginManager().getPlugin("SafeJoin").getDescription().getVersion() + "==");
+            sender.sendMessage(ChatColor.GOLD + "[SafeJoin] " + ChatColor.WHITE + "/safejoin tptospawn [player]");
+            sender.sendMessage(ChatColor.GOLD + "[SafeJoin] " + ChatColor.WHITE + "/safejoin reload");
+            sender.sendMessage(ChatColor.GOLD + "[SafeJoin] " + ChatColor.WHITE + "by cylin https://github.com/cyilin");
         }
         return true;
     }
@@ -54,21 +62,23 @@ public class Main extends JavaPlugin implements Listener, CommandExecutor {
     public void onDisable() {
         PlayerJoinEvent.getHandlerList().unregister((JavaPlugin) this);
         this.worlds = null;
-        this.timestamp = new HashMap();
+        this.timestamp = null;
         this.ess = null;
+        tp.clear();
     }
 
-    public void reload() {
+    private void reload() {
         onDisable();
         onEnable();
     }
 
-    public Date stringToDate(String s) {
+    private Date stringToDate(String s) {
         Date date = new Date();
         DateFormat d = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         try {
             date = d.parse(s);
         } catch (Exception e) {
+            d.format(System.currentTimeMillis());
         }
         return date;
     }
@@ -78,9 +88,7 @@ public class Main extends JavaPlugin implements Listener, CommandExecutor {
         saveDefaultConfig();
         try {
             getConfig().load(new File(getDataFolder(), "config.yml"));
-        } catch (IOException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (InvalidConfigurationException ex) {
+        } catch (IOException | InvalidConfigurationException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
         getCommand("safejoin").setExecutor(this);
@@ -93,6 +101,8 @@ public class Main extends JavaPlugin implements Listener, CommandExecutor {
         this.mode = getConfig().getInt("mode");
         this.worlds = getConfig().getStringList("worlds");
         this.command = getConfig().getString("command");
+        timestamp = new HashMap();
+        tp = new HashSet<>();
         this.load();
     }
 
@@ -104,23 +114,28 @@ public class Main extends JavaPlugin implements Listener, CommandExecutor {
         Player player = event.getPlayer();
         Long logout = ess.getUser(player).getLastLogout();
         String world_name = player.getLocation().getWorld().getName();
+        if (!tp.isEmpty() && tp.contains(player.getName().toLowerCase())) {
+            player.teleport(getServer().getWorld("world").getSpawnLocation());
+            tp.remove(player.getName().toLowerCase());
+        }
         if (worlds.contains(world_name)) {
             if (logout < this.timestamp.get(world_name)) {
+                getLogger().info(String.format("player:%s world:%s loc:%s %s %s", player.getName(), world_name, player.getLocation().getX(), player.getLocation().getY(), player.getLocation().getZ()));
                 if (this.mode == 1) {
                     player.setNoDamageTicks(100);
-                    getLogger().log(Level.INFO, "{0} {1} {2}", new Object[]{world_name,player.getName(), this.command});
+                    getLogger().log(Level.INFO, "{0} {1} {2}", new Object[]{world_name, player.getName(), this.command});
                     getServer().dispatchCommand(player, this.command);
                 } else if (this.mode == 2) {
                     player.setNoDamageTicks(100);
                     String c = this.command.replace("{player}", player.getName());
-                    getLogger().log(Level.INFO, "{0} {1} {2}", new Object[]{world_name,player.getName(), c});
+                    getLogger().log(Level.INFO, "{0} {1} {2}", new Object[]{world_name, player.getName(), c});
                     getServer().dispatchCommand(getServer().getConsoleSender(), c);
                 }
             }
         }
     }
 
-    public void load() {
+    private void load() {
         ess = (IEssentials) getServer().getPluginManager().getPlugin("Essentials");
         FileConfiguration data = YamlConfiguration.loadConfiguration(new File(getDataFolder(), "data.yml"));
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -132,8 +147,8 @@ public class Main extends JavaPlugin implements Listener, CommandExecutor {
         } catch (ParseException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
-        for (Iterator<World> world = getServer().getWorlds().iterator(); world.hasNext();) {
-            String world_name = world.next().getName();
+        for (World world1 : getServer().getWorlds()) {
+            String world_name = world1.getName();
             if (!worlds.contains(world_name)) {
                 continue;
             }
@@ -145,7 +160,6 @@ public class Main extends JavaPlugin implements Listener, CommandExecutor {
             } else {
                 this.timestamp.put(world_name, this.stringToDate(data.getString(world_name + ".date")).getTime());
             }
-
         }
         try {
             data.save(new File(getDataFolder(), "data.yml"));
